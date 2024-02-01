37aab7df63af5a8db9bcdf3f119a1601a4c11a5a
Abhishek-Chenchety <chenchetyabhishek@gmail.com>
2024-02-01 15:39:27 +0530

    Implement case conditions instead of if else

--- Q2.v
+++ Q2.v
@@ -9,21 +9,28 @@ input [3:0] x_0, x_1, x_2, x_3, x_4,x_5, x_6, x_7, x_8, x_9, h_0, h_1, h_2, h_3,
 
 reg [7:0] m0,m1,m2,m3,m4,m5,m6,m7,m8,m9;  
 
-always @(posedge clock) begin
-        if(counter[0]==1'b0) begin
-            m0<=x_0*h_0;         
-            m1<=x_1*h_1;         
-            m2<=x_2*h_2;         
-            m3<=x_3*h_3;         
-            m4<=x_4*h_4;         
-            m5<=x_5*h_5;         
-            m6<=x_6*h_6;         
-            m7<=x_7*h_7;         
-            m8<=x_8*h_8;         
-            m9<=x_9*h_9;
-        end else begin
-            out <= m0+m1+m2+m3+m4+m5+m6+m7+m8+m9;
+always @(posedge clock,reset) begin  
+        if(reset) begin
+            out<=0;
+        end  
+        else begin
+            case (counter[0]&1)
+                1'b0: begin
+                 m0<=x_0*h_0;         
+                 m1<=x_1*h_1;         
+                 m2<=x_2*h_2;         
+                 m3<=x_3*h_3;         
+                 m4<=x_4*h_4;         
+                 m5<=x_5*h_5;         
+                 m6<=x_6*h_6;         
+                 m7<=x_7*h_7;         
+                 m8<=x_8*h_8;         
+                 m9<=x_9*h_9;
+                end
+                1'b1: out <= m0+m1+m2+m3+m4+m5+m6+m7+m8+m9;
+                default: out <= 0; 
+            endcase
+            counter <= counter+1;
         end
-        counter <= counter+1;        
 end
 endmodule
